5d90a1fb0f13282c7b932f24042b2ddfb48a040d
hiropppp <hrkysd.yoshida@gmail.com>
2021-10-20 16:48:25 +0900

    テストソースとスクレイピングソースの追加

---
+++ scrape_do/config.py
@@ -0,0 +1,8 @@
+API_TOKEN = 'Your_API_TOKEN_FOR_scrape.do'
+URL = 'https://docs.scrape.do/'
+METHOD = 'GET'
+PAYLOAD = {}
+HEADERS = {}
+RENDER = False
+SUPER_PROXIES = False
+GEO_CODE = None
---
+++ test.py
@@ -0,0 +1,35 @@
+import traceback
+
+from scrape_do import *
+
+# create an web scrapper object
+sample = python_sample()
+
+# set the scrape.do api key
+sample.set_api_token(api_token=API_TOKEN)
+
+# Get Scrape.do account statistics
+try:
+    resp = sample.account_status()
+    print("Response Type " + str(type(resp)))
+    print(resp)
+except ConnectionError as e:
+    print(str(e))
+    print(traceback.format_exc())
+
+except Scrape_do_Exception as e:
+    print(str(e))
+    print(traceback.format_exc())
+
+try:
+    resp = sample.create_request_url(url='https://docs.scrape.do/', method="GET", payload={}, headers={},
+                                     render=False, super_proxies=False, geo_code=None)
+    print(resp)
+except ConnectionError as e:
+    print(str(e))
+    print(traceback.format_exc())
+
+except Scrape_do_Exception as e:
+    print(str(e))
+    print(traceback.format_exc())
+
